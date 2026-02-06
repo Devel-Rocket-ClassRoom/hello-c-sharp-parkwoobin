1341b7ff94c719bf8cc3000e25a9895df1fa5cc8
parkwoobin <oi3oi3oi@naver.com>
2026-02-06 16:11:17 +0900

    Program.cs 출력, git ignore 추가

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection.PortableExecutable;
+
+class MainClass
+{
+    static void Main(string[] args)
+    {
+        bool boolValue = true;
+        int intValue = 10;
+        float floatValue = 3.4f;
+        char charValue = 'A';
+
+        Console.WriteLine("boolValue: " + boolValue.ToString());
+        Console.WriteLine(intValue);
+        Console.WriteLine(floatValue);
+        Console.WriteLine(charValue);
+    }
+}
